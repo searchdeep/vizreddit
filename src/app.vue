197641deea0af0c8d4f545678696b294d4df99c7
Deepak Manoharan <deepak@searchera.io>
2017-05-03 13:44:06 -0700

    added about modal

--- src/app.vue
+++ src/app.vue
@@ -11,7 +11,9 @@
               <span class="icon-bar"></span>
             </button>
 
-            <a class="navbar-brand" href="#/">Reddit Visualization</a>
+            <a class="navbar-brand" href="/">Reddit Visualization</a>
+            <button class="btn btn-primary btn-head pull-right" data-toggle="modal" data-target="#myModal">About</button>
+            <a class="navbar-brand pull-right" target="_blank" href="https://searchera.io">Powered by Searchera.io</a>
           </div>
 
           <!--
@@ -27,6 +29,30 @@
           -->
       </div>
     </div>
+
+    <div id="myModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Reddit Visualization</h4>
+          </div>
+          <div class="modal-body">
+            <p>This visualization is powered by <a href="https://searchera.io" target="_blank">Searchera</a> hosted search API.</p>
+            <p>Thanks to <a href="https://reddit.com/u/stuck_in_the_matrix" target="_blank">/u/stuck_in_the_matrix</a> for collecting the data and <a href="https://reddit.com/u/fhoffa" target="_blank">/u/fhoffa</a> for loading it on Google BigQuery</p>
+            <p>The data mirrored on Searchera is hosted in New York.  Performance is not ideal outside of USA and parts of Europe.</p>
+            <p>If you are interested in just search results, please use <a href="http://searchreddit.net">http://searchreddit.net</a></p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
     <router-view></router-view>
   </div>
 </template>
@@ -38,9 +64,23 @@ export default {
   name: 'app',
   components: {
     reddit
+  },
+  data () {
+    return {
+      showModal: false
+    }
   }
 }
 </script>
 
 <style>
+.navbar-header{
+  margin-left:5px;
+  width:100%;
+}
+.btn-head {
+  margin-top: 4px;
+  padding: 5px 15px;
+  font-weight: 600;
+}
 </style>
